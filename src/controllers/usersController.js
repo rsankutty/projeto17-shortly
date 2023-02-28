156db25bf6a81054f7e19196cdb086002cd44ba7
rsankutty <r.sankutty@gmail.com>
2023-02-28 00:47:37 -0300

    fixed ranking query

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -50,9 +50,9 @@ export async function getUserInfo(req, res) {
                 users.id,
                 users.name,
                 COUNT(urls.id) AS "linksCount",
-                SUM(urls.visitCount) AS "visitCount"
+                SUM(urls."visitCount") AS "visitCount"
             FROM users
-            LEFT JOIN urls ON users.id = urls.userId 
+            LEFT JOIN urls ON users.id = urls."userId" 
             GROUP BY users.id
             ORDER BY "visitCount" DESC
             LIMIT 10;          
